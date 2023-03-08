7178ee4ec361b160528a8312271ca8ad1a9ecb38
vinni-44 <vinod.art44@gmail.com>
2023-03-08 23:05:07 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'winequality',
+    version = '0.0.1',
+    author = 'vinni-44',
+    description = 'its a wine quality package',
+    packages = find_packages(),
+    license = 'MIT'
+)
