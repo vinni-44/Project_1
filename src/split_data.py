0d6a6ed76773b1db31e0fc5f906ac6b8407e7cd8
vinni-44 <vinod.art44@gmail.com>
2023-03-08 15:16:43 +0530

    stage 2 complete

---
+++ src/split_data.py
@@ -0,0 +1,27 @@
+# split the raw data 
+# save it in data/processed folder
+import os
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from get_data import read_params
+
+def split_anda_saved_data(config_path):
+    config = read_params(config_path)
+    train_data_path = config['split_data']['train_path']
+    test_data_path = config['split_data']['test_path']
+    raw_data_path = config['load_data']['raw_dataset_csv']
+    split_ratio  = config['split_data']['test_size']
+    random_state = config['base']['random_state']
+
+    df = pd.read_csv(raw_data_path,sep=',',encoding='utf-8')
+    train,test = train_test_split(df,test_size=split_ratio,random_state=random_state)
+
+    train = train.to_csv(train_data_path, sep =',',index = False, encoding = 'utf-8')
+    test = test.to_csv(test_data_path, sep=',',index = False, encoding = 'utf-8')
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="paramal.yaml")
+    parsed_args = args.parse_args()
+    split_anda_saved_data(config_path=parsed_args.config)
